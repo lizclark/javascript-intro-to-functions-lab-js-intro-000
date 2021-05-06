f41b2999887be03f697333e67d9e1fa2083f169a
Liz Clark <ebclark917@gmail.com>
2021-05-06 22:18:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function logWhisper (string) {
 logWhisper ('HELLO')
 
 function sayHiToGrandma (string) {
-  if ('hello' === string.toLowerCase)
+  if ('hello') === string.toLowerCase)
   return "I can't hear you!";
   else if ('HELLO') === string.toUpperCase)
   return "YES INDEED!";
