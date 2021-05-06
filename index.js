c9ee55573340208b53b950c21a5fc16ed093433c
Liz Clark <ebclark917@gmail.com>
2021-05-06 22:25:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,18 +14,19 @@ function logShout (string) {
 logshout('hello')
 
 function logWhisper (string) {
-  return console.log(string.toLowerCase)
+  console.log('hello')
+  return string.toLowerCase
 }
 
 logWhisper ('HELLO')
 
 function sayHiToGrandma (string) {
-  if (string === string.toLowerCase)
-  return "I can't hear you!";
-  else if (string === string.toUpperCase)
-  return "YES INDEED!";
-  else if (string === "I love you, Grandma.")
-  return "I love you, too."
+  if (string === string.toLowerCase){ 
+    return "I can't hear you!"}  
+  else if (string === string.toUpperCase){
+    return "YES INDEED!"}
+else if (string === "I love you, Grandma."){
+  return "I love you, too."}
 }
 sayHiToGrandma('hello');
 sayHiToGrandma('HELLO');
